d2dac93599a4f5d281270bd40352c773a1828e7f
MBriggs404 <17Briggs_M@bexleygs.co.uk>
2023-04-27 11:01:44 +0100

    stack practice 1.0

---
+++ src/com/company/ListNode.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class ListNode {  private int value;
+    private ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setNext(ListNode pointer) {
+        next = pointer;
+    }
+
+    public void setValue(int newValue) {
+        value = newValue;
+    }
+}
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] testValues = {13,7,19,68};
+        List testList = new List(testValues);
+        testList.display();
+
+    }
+}
